d5bfefed1cbb7cf5a35322462b999ec7d08556f3
Shaik Rafiya <shaikrafiya@Shaiks-MacBook-Pro.local>
2026-01-30 22:46:02 +0530

    handles if user already exist
    * createUser returns response back

--- src/api.js
+++ src/api.js
@@ -1,6 +1,30 @@
+const createSuccessResponse = (body) => ({
+  success: true,
+  body,
+  error: {},
+});
+
+const createFailureResponse = (error) => ({
+  success: false,
+  body: {},
+  error,
+});
+
 export const createUser = (users, request) => {
+  if (request.userId in users) {
+    return createFailureResponse({
+      errorCode: 10,
+      errorMessage: `Error: userId ${request.userId} already exist`,
+    });
+  }
+
   users[request.userId] = {
     userName: request.userName,
     password: request.password,
   };
+
+  return createSuccessResponse({
+    userId: request.userId,
+    userName: users[request.userId].userName,
+  });
 };
--- test/api_test.js
+++ test/api_test.js
@@ -13,7 +13,43 @@ describe("typing-monkey", () => {
         password: "12345",
         userId: "someone123",
       };
-      createUser(users, request);
+      assertEquals(createUser(users, request), {
+        success: true,
+        body: {
+          userId: "someone123",
+          userName: "someone",
+        },
+        error: {},
+      });
+      assertEquals(users, {
+        "someone123": { userName: "someone", password: "12345" },
+      });
+    });
+    it("user already exist", () => {
+      const request = {
+        userName: "someone",
+        password: "12345",
+        userId: "someone123",
+      };
+      assertEquals(createUser(users, request), {
+        success: true,
+        body: {
+          userId: "someone123",
+          userName: "someone",
+        },
+        error: {},
+      });
+      assertEquals(users, {
+        "someone123": { userName: "someone", password: "12345" },
+      });
+      assertEquals(createUser(users, request), {
+        success: false,
+        body: {},
+        error: {
+          errorCode: 10,
+          errorMessage: `Error: userId ${request.userId} already exist`,
+        },
+      });
       assertEquals(users, {
         "someone123": { userName: "someone", password: "12345" },
       });
